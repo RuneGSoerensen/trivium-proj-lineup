193d7feced3f5fa429ba7a7fb0e020303c1b70c4
Morten Kirch Pedersen <morten-kris@live.dk>
2025-11-21 12:32:55 +0100

    created getUserData
    Created getUserData endpoint to fetch all data related to a user, used json_agg to aggregate arrays like genres, tags, and questions, and jsonb_build_object to structure socials as JSON objects.

--- backend/modules/user/controller.js
+++ backend/modules/user/controller.js
@@ -45,3 +45,51 @@ export const createUser = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+export const getUserData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [user] = await sql`
+  SELECT 
+    u.id,
+    u.name,
+    u.business,
+    u.bio,
+    u.about,
+    u.theme,
+    u.image_url,
+
+    (SELECT jsonb_agg(jsonb_build_object(
+        'spotify', s.spotify,
+        'instagram', s.instagram,
+        'x', s.x,
+        'youtube', s.youtube,
+        'tiktok', s.tiktok
+    ))
+     FROM socials s
+     WHERE s.user_id = u.id
+    ) AS socials,
+
+    json_agg(DISTINCT g.name) AS genres,
+
+    json_agg(DISTINCT lft.name) AS looking_for_tags,
+
+    json_agg(DISTINCT jsonb_build_object('question', q.question, 'answer', uq.answer)) AS questions
+
+FROM users u
+LEFT JOIN socials s ON s.user_id = u.id
+LEFT JOIN user_genres ug ON ug.user_id = u.id
+LEFT JOIN genres g ON g.id = ug.genre_id
+LEFT JOIN looking_for lf ON lf.user_id = u.id
+LEFT JOIN looking_for_tags lft ON lft.id = lf.tag_id
+LEFT JOIN user_questions uq ON uq.user_id = u.id
+LEFT JOIN questions q ON q.id = uq.question_id
+
+WHERE u.id = ${id}
+GROUP BY u.id;
+    `;
+    return res.json({ user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
