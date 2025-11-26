f39800cf6d6952a6ef0dc191ca39c918f82b9809
Rune Gerner Sørensen <143501521+RuneGSoerensen@users.noreply.github.com>
2025-11-26 11:46:37 +0100

    Update backend/server.js
    Patch is added to HTTPS methods for server.js
    
    Co-authored-by: Morten Kirch Pedersen <143696818+MortenKirch@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(
   cors({
     origin: "http://localhost:3000", // Adjust this to your frontend's origin
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE, PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
