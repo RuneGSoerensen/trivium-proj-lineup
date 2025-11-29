47c2febc88723c06dabc0d69041d5bc579277fb5
vicwaet <vicwaet@hotmail.com>
2025-11-29 15:26:04 +0100

    Update documentation and chat imports

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import notesRouter from "./modules/notes/router.js";
 import lookingForTagsRouter from "./modules/looking_for/router.js";
 import { requireAuth } from "./middelware/auth.js";
 import genreRouter from "./modules/genres/router.js";
+import chatRouter from "./modules/chat/router.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Before production this needs to be changed to a valid url, or something more secure.
