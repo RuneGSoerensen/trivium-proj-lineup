dd6f032267c6c43bfa409c9bb3fa373c4085abb3
vicwaet <vicwaet@hotmail.com>
2025-12-02 13:56:06 +0100

    Refactor: Update layout styles for improved structure and padding

--- frontend/app/(pages)/chat/layout.jsx
+++ frontend/app/(pages)/chat/layout.jsx
@@ -17,7 +17,7 @@ export default function ChatLayout({ children }) {
 
     return (
         <ChatHeaderContext.Provider value={{ ...headerState, setHeaderState }}>
-            <section className="flex flex-col w-full h-full justify-between items-center pt-24 bg-brand-secondary">
+            <section className="paddingNone flex flex-col w-full h-full justify-between items-center pt-24 bg-brand-secondary">
                 <div className='flex w-full justify-between py-8 px-22 items-center'>
                     {mode === 'list' ? (
                         <>
