e2160b98d78375b064ea8cfdd9e50ae55c7d158d
badgerson <mail@badgerson.com>
2025-12-03 12:36:56 +0100

    request body handling for POST /requests

--- backend/modules/requests/controller.js
+++ backend/modules/requests/controller.js
@@ -2,4 +2,36 @@ import sql from "../../db.js";
 import z from 'zod';
 
 export async function createRequest(req, res) {
+  const userId = req.userId;
+
+  const schema = z.object({
+    title: z.string(),
+    description: z.string(),
+    paid_opportunity: z.boolean(),
+    image_url: z.url().default(null),
+    location: z.string().default(null),
+    people_user_ids: z.array(z.uuid()).default([]),
+    genres: z.array(z.string()).default([]),
+  });
+
+  const result = schema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json(
+      { error: result.error.issues }
+    );
+  }
+
+  console.log(result.data);
+
+  const {
+    title,
+    description,
+    paid_opportunity,
+    image_url,
+    location,
+    people_user_ids,
+    genres,
+  } = result.data;
+
 }
