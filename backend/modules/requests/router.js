bb4aef369e41c3d0815e4fa8bbfa53bb7ca40f19
Morten Kirch Pedersen <morten-kris@live.dk>
2025-12-05 10:22:41 +0100

    created server route to get all requests

--- backend/modules/requests/router.js
+++ backend/modules/requests/router.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createRequest } from './controller.js';
+import { createRequest, getAllRequests } from './controller.js';
 import { requireAuth } from '../../middelware/auth.js';
 
 const router = express.Router();
 
 // Create a request
 router.post('/', requireAuth, createRequest);
+
+router.get('/getAll', getAllRequests);
 
 export default router;
