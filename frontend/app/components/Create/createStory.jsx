72ff09874f58f68ba0669e4f22cb1a4f97220deb
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2025-12-09 09:33:13 +0100

    Implement coming soon

--- frontend/app/components/Create/createStory.jsx
+++ frontend/app/components/Create/createStory.jsx
@@ -1,6 +1,6 @@
 export default function CreateStory({ userName, userImage }) {
   return (
-    <div className="flex flex-col items-center w-full h-full">
+    <div className="flex flex-col bg-slate-500 text-zinc-950 w-300 h-300 rounded-lg p-4 items-center justify-center">
       <h1>Create Story Section</h1>
     </div>
   );
