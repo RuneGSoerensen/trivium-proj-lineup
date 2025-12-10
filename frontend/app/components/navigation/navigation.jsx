d92a975f8648e4b3cb6ea92f3f57255cf0df69f1
vicwaet <vicwaet@hotmail.com>
2025-12-10 16:34:18 +0100

    refactor: update glass utility class to trvm-glass-dark for consistent styling

--- frontend/app/components/navigation/navigation.jsx
+++ frontend/app/components/navigation/navigation.jsx
@@ -40,7 +40,7 @@ export default function Navigation() {
     // Nav container
     // Remove Change color secondary darkgrey
     <nav className="fixed bottom-28 left-0 right-0 flex w-full justify-center">
-      <div className="flex items-center w-fit justify-center rounded-pill glass">
+      <div className="flex items-center w-fit justify-center rounded-pill trvm-glass-dark">
         {/* Mapped links */}
         {links.map((link) => (
           <NavLink
