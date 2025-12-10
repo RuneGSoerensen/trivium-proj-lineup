026cd53f4db32e44d1c9035d2d0b968398b60d5e
vicwaet <vicwaet@hotmail.com>
2025-12-10 17:40:23 +0100

    refactor: remove unused SearchUser component; enhance Stories component with user avatar initials and improved layout and styling

--- frontend/app/components/homepage/stories.jsx
+++ frontend/app/components/homepage/stories.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { userAvatarInitials } from "@/utils/helpers";
 export default function Stories() {
   const [stories, setStories] = useState([]);
   const fetchStories = async () => {
@@ -27,27 +28,28 @@ export default function Stories() {
     loadStories();
   }, []);
 
+  const defaultAvatarStyle = "background=ffcf70&color=1e1e1e&size=40";
 
   return (
-    <section className="w-full flex gap-4 overflow-x-auto py-4 px-2">
+    <section className="w-full h-fit flex gap-12 overflow-x-auto py-12 px-16 full-bleed">
       {stories.map((story) => (
-        <div key={story.id} className=" flex  ">
-          <div>
+        <div key={story.id} className="flex flex-col items-center gap-6 h-full">
+   
             <div className="rounded-full w-70 h-70">
               <Image
                 src={
                   story.image_url && story.image_url !== ""
                     ? story.image_url
-                    : "/placeholder-image.png"
+                    : `https://ui-avatars.com/api/?name=${encodeURIComponent(userAvatarInitials(story.name))}&${defaultAvatarStyle}&size=128`
                 }
                 alt={story.name}
-                className=" w-70 h-70 rounded-full object-cover border-1 border-primary"
+                className=" w-74 h-74 rounded-full object-cover border-brand"
                 width={48}
                 height={48}
               />
             </div>
-            <p className="text-sm text-center">{story.name}</p>
-          </div>
+            <p className="text-sm text-center leading-16!">{story.name}</p>
+    
         </div>
       ))}
     </section>
