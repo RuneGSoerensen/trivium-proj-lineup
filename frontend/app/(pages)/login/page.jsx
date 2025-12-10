45211915b0c3194388b4edda47ab3afd40371d86
vicwaet <vicwaet@hotmail.com>
2025-12-10 09:44:22 +0100

    add missing import

--- frontend/app/(pages)/login/page.jsx
+++ frontend/app/(pages)/login/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { signInWithPassword } from "@/utils/supabaseClient";
 import Input from "@/ui/Input/Input";
